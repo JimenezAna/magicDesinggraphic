d31c032b23e3d6597c365ba9f1964aaaff5b9952
JimenezAna <109441556+JimenezAna@users.noreply.github.com>
2022-07-16 18:00:40 -0500

    Magic
    aqui

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+	document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
+ScrollReveal().reveal('.social',{delay:500});
+ScrollReveal().reveal('.news-cards-nosotros',{delay:500});
+ScrollReveal().reveal('.project-img',{delay:500});
+ScrollReveal().reveal('.sucursales-img',{delay:500});
